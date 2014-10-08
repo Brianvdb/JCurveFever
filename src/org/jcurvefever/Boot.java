9e80ad2b2662fc4f75659ba42be5e4e2e93b1c36
stanvanheumen <cjgheume@student.avans.nl>
2014-10-08 10:47:16 +0200

    Made important changes.

--- src/org/jcurvefever/Boot.java
+++ src/org/jcurvefever/Boot.java
@@ -2,8 +2,8 @@ package org.jcurvefever;
 
 public class Boot {
 
-	public static void main(String... args) {
-
+	public static void main(String[] args) {
+		System.out.println("Hello World!");
 	}
 
 }
